0d5034cf65c289ddadb59664d0fcfa8366bb7a32
Yair Shefi <yayo.shefi@gmail.com>
2025-03-13 12:32:37 +0200

    add tile dataloader for all the tiles

--- tile_dataset.py
+++ tile_dataset.py
@@ -12,21 +12,49 @@ from albumentations.pytorch import ToTensorV2
 from python_wsi_preproc import slide, tiles
 from python_wsi_preproc.tiles import TileSummary, Tile
 
-class TileDataset(IterableDataset):
+class TileDataset(Dataset):
     
-    def __init__(self, data, tile_summaries, transform=None):
-        self.data = data
-        self.tile_summaries = tile_summaries
+    def __init__(self, slides_df, tiles_df, transform=None):
+        self.slides_df = slides_df
+        self.tiles_df = tiles_df
         self.transform = transform
-        self.data['num_of_high'] = self.data['slide_num'].apply(lambda x: self.tile_summaries[x].high)
-        self.data['num_of_medium'] = self.data['slide_num'].apply(lambda x: self.tile_summaries[x].medium)
-        self.data['num_of_low'] = self.data['slide_num'].apply(lambda x: self.tile_summaries[x].low)
+
+        # commons
+        self.tile_drop_cols = self.tiles_df.columns.intersection(["rank", "split"])
 
     @classmethod
     def generate_dataset(cls, csv_file, transform=None) -> 'TileDataset':
         data = pd.read_csv(csv_file)
         tile_summaries = cls._create_tile_summaries(data)
-        return cls(data, tile_summaries, transform)
+        
+        tiles_df = pd.DataFrame()
+        tile_summaries_records = []
+
+        for _, row in data.iterrows():
+            slide_num = row['slide_num']
+            ts = tile_summaries[slide_num]
+            tile_sumary_record = dict(
+                slide_num=ts.slide_num,
+                orig_w=ts.orig_w, orig_h=ts.orig_h,
+                orig_tile_w=ts.orig_tile_w, orig_tile_h=ts.orig_tile_h,
+                scaled_w=ts.scaled_w, scaled_h=ts.scaled_h,
+                scaled_tile_w=ts.scaled_tile_w, scaled_tile_h=ts.scaled_tile_h,
+                tissue_percentage=ts.tissue_percentage,
+                num_col_tiles=ts.num_col_tiles, num_row_tiles=ts.num_row_tiles,
+                # extended attributes
+                count=ts.count, high=ts.high, medium=ts.medium, low=ts.low, none=ts.none,
+            )
+            tile_summaries_records.append(tile_sumary_record)
+
+            tmp_tiles = pd.DataFrame.from_records([tile.__dict__ for tile in ts.tiles])
+            tmp_tiles["tile_summary"] = None
+            # tmp_tiles.rename(columns={'tissue_percentage': 't_p'}, inplace=True)
+            tiles_df = pd.concat([tiles_df, tmp_tiles], ignore_index=True)
+            
+        tiles_summary_df = pd.DataFrame.from_records(tile_summaries_records)
+        slides_df = data.merge(tiles_summary_df, on="slide_num", how="left")
+
+        return cls(slides_df, tiles_df, transform)
     
     @staticmethod
     def _create_tile_summaries(data):
@@ -47,79 +75,34 @@ class TileDataset(IterableDataset):
         Args:
             dst_dir (str): directory path to dump the files
         """
-        tiles_df = pd.DataFrame()
-        tile_summaries_records = []
-
-        for _, row in self.data.iterrows():
-            slide_num = row['slide_num']
-            ts = self.tile_summaries[slide_num]
-            tile_sumary_record = dict(
-                slide_num=ts.slide_num,
-                orig_w=ts.orig_w, orig_h=ts.orig_h,
-                orig_tile_w=ts.orig_tile_w, orig_tile_h=ts.orig_tile_h,
-                scaled_w=ts.scaled_w, scaled_h=ts.scaled_h,
-                scaled_tile_w=ts.scaled_tile_w, scaled_tile_h=ts.scaled_tile_h,
-                tissue_percentage=ts.tissue_percentage,
-                num_col_tiles=ts.num_col_tiles, num_row_tiles=ts.num_row_tiles,
-                # extended attributes
-                count=ts.count, high=ts.high, medium=ts.medium, low=ts.low, none=ts.none,
-            )
-            tile_summaries_records.append(tile_sumary_record)
-
-            tmp_tiles = pd.DataFrame.from_records([tile.__dict__ for tile in ts.tiles])
-            tmp_tiles.rename(columns={'tissue_percentage': 't_p'}, inplace=True)
-            tiles_df = pd.concat([tiles_df, tmp_tiles], ignore_index=True)
-            
-        tiles_summary_df = pd.DataFrame.from_records(tile_summaries_records)
-        
-        tiles_df.to_pickle(os.path.join(dst_dir, 'tiles.pkl'))
-        tiles_summary_df.to_pickle(os.path.join(dst_dir, 'tiles_summary.pkl'))
+        self.tiles_df.to_pickle(os.path.join(dst_dir, 'tiles.pkl'))
+        self.slides_df.to_pickle(os.path.join(dst_dir, 'slides.pkl'))
 
     @classmethod
-    def load_dataset(cls, csv_file:str, src_dir: str, transform=None) -> 'TileDataset':
-        data = pd.read_csv(csv_file)
-
+    def load_dataset(cls, src_dir: str, transform=None) -> 'TileDataset':
         tiles_df = pd.read_pickle(os.path.join(src_dir, 'tiles.pkl'))
-        tiles_summary_df = pd.read_pickle(os.path.join(src_dir, 'tiles_summary.pkl'))
+        slides_df = pd.read_pickle(os.path.join(src_dir, 'slides.pkl'))
 
-        tile_summaries = {}
-        for _, row in tiles_summary_df.iterrows():
-            ts = TileSummary(row['slide_num'], row['orig_w'], row['orig_h'], row['orig_tile_w'], row['orig_tile_h'],
-                             row['scaled_w'], row['scaled_h'], row['scaled_tile_w'], row['scaled_tile_h'],
-                             row['tissue_percentage'], row['num_col_tiles'], row['num_row_tiles'])
-            ts.count = row['count']
-            ts.high = row['high']
-            ts.medium = row['medium']
-            ts.low = row['low']
-            ts.none = row['none']
+        return cls(slides_df, tiles_df, transform)
 
-            slide_tile_df = tiles_df[tiles_df['slide_num'] == ts.slide_num]
-            tiles = [Tile(**tile) for _, tile in slide_tile_df.drop(columns=["rank"]).iterrows()]
-            for tile in tiles:  # fill ranks (which are not part of the constructor)
-                tile.rank = slide_tile_df[slide_tile_df.tile_num==tile.tile_num]['rank'].values[0]
-            ts.tiles = tiles
-            tile_summaries[ts.slide_num] = ts
-        return cls(data, tile_summaries, transform)
+    def __len__(self):
+        return len(self.tiles_df)
 
-
-    # TODO: replace the iteratable dataset with a regular dataset and then change the __get_item__ / iter to make sure we take even number of pos, negative
-    def __iter__(self):
-        while True:
-            for _, row in self.data.iterrows():
-                slide_num = row['slide_num']
-                label = row['tag']
-                tile_summary = self.tile_summaries[slide_num]
-                tile = random.choice(tile_summary.tiles)
-                image = tile.get_np_tile()
-                
-                data = dict(slide_num=slide_num, tile_row=tile.r, tile_col=tile.c, label=label)
-                data.update(dict(tissue_percent=tile.tissue_percentage, score=float(tile.score)))  # Main scores
-                data.update(dict(color_factor=float(tile.color_factor), s_and_v_factor=tile.s_and_v_factor, quantity_factor=tile.quantity_factor))
-
-                if self.transform:
-                    image = self.transform(image=image)['image']
-                yield image, label, data
-
+    def __getitem__(self, index):
+        tile_srs = self.tiles_df.iloc[index]
+        tile_srs = tile_srs.rename({'tissue_percentage': 't_p'}).drop(self.tile_drop_cols)
+        tile = Tile(**tile_srs)
+        image = tile.get_np_tile()
+        label = self.slides_df[self.slides_df.slide_num == tile.slide_num].tag.values[0]
+        
+        data = dict(slide_num=tile.slide_num, tile_row=tile.r, tile_col=tile.c, label=label)
+        data.update(dict(tissue_percent=tile.tissue_percentage, score=float(tile.score)))  # Main scores
+        data.update(dict(color_factor=float(tile.color_factor), s_and_v_factor=tile.s_and_v_factor, quantity_factor=tile.quantity_factor))
+        
+        if self.transform:
+            image = self.transform(image=image)['image']
+        return image, label, data
+    
         
 def collate_fn(batch):
     images, labels, data = zip(*batch)
@@ -146,13 +129,13 @@ test_transform = A.Compose([
 
 # Build DataLoader
 # ================
-def create_dataloader(csv_file, batch_size=32, num_workers=4, is_train=True):
+def create_dataloader(src_dir, batch_size=32, num_workers=4, is_train=True):
     transform=train_transform if is_train else test_transform
-    dataset = TileDataset(csv_file, transform=transform)
+    dataset = TileDataset.load_dataset(src_dir=src_dir, transform=transform)
     dataloader = DataLoader(
         dataset, batch_size=batch_size,
         num_workers=num_workers,
-        # shuffle=True if is_train else False,
+        shuffle=True if is_train else False,
         collate_fn=collate_fn
         )
     return dataloader
@@ -162,24 +145,21 @@ if __name__ == "__main__":
     import time
     # Test the dataloader
     tic = time.time()
-    dataloader = create_dataloader('data/datasets/tupac_16_100/data.csv', batch_size=32, num_workers=0, is_train=True)
+    all_csv = 'data/datasets/tupac_16/data_full.csv'
+    test_csv = 'data/datasets/tupac16/data_test.csv'
+    dataset = TileDataset.generate_dataset(csv_file=test_csv, transform=train_transform)
+    dataloader = DataLoader(dataset, batch_size=32, num_workers=0, shuffle=True, collate_fn=collate_fn)
     toc = time.time()
     print(f"Time to create dataloader: {toc-tic:.2f} seconds")
 
     # Dataset Stats:
-    df = dataloader.dataset.data
-    min_width, min_height = tiles.ROW_TILE_SIZE, tiles.COL_TILE_SIZE
-    for row in df.itertuples():
-        tile_sum = dataloader.dataset.tile_summaries[row.slide_num]
-        for tile in tile_sum.tiles:
-            width = tile.o_c_e - tile.o_c_s
-            min_width = min(min_width, width)
-            height = tile.o_r_e - tile.o_r_s
-            min_height = min(min_height, height)
-        df.loc[row.Index, 'min_width'] = min_width
-        df.loc[row.Index, 'min_height'] = min_height
-    print(df)
-
+    tmp = dataset.tiles_df.assign(
+            width=lambda tile: tile.o_c_e - tile.o_c_s,
+            height=lambda tile: tile.o_r_e - tile.o_r_s,
+    )
+    stats_df = tmp.groupby("slide_num").min(["width", "height"])
+    print(stats_df)
+    
     for i, batch in enumerate(dataloader):
         print(f"Batch {i}: {len(batch)} entities, and {len(batch[0])} images")
         print(batch[0].shape, batch[1].shape)
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 from lit_resnet import LitResnet
 from wsi_dataset import create_dataloader as create_wsi_dataloader
+from infinite_tile_dataset import create_dataloader as create_infinite_tile_dataloader
 from tile_dataset import create_dataloader as create_tile_dataloader
 import argparse
 import torch
@@ -13,21 +14,17 @@ logger = logging.getLogger(__name__)
 
 def main(args):
     # Data loaders
-    train_csv = os.path.join('data/datasets', args.train_csv)
-    val_csv = os.path.join('data/datasets', args.val_csv)
-    logger.info(f"Construction DataLoader with train_csv: {train_csv}, val_csv: {val_csv}")
-    # train_loader = create_wsi_dataloader(
+    train_dir = os.path.join('data/datasets', args.train_path)
+    val_dir = os.path.join('data/datasets', args.val_path)
+    logger.info(f"Construction DataLoader with train: {train_dir}, val: {val_dir}")
+    # train_dataloader = create_wsi_dataloader(
     #     csv_file=train_csv, data_dir='data/raw/training_png', batch_size=args.batch_size, is_train=True
     # )
-    # val_loader = create_wsi_dataloader(
+    # val_dataloader = create_wsi_dataloader(
     #     csv_file=val_csv, data_dir='data/raw/training_png', batch_size=args.batch_size, is_train=False
     # )
-    train_loader = create_tile_dataloader(
-        csv_file=train_csv, batch_size=args.batch_size, is_train=True
-    )
-    val_loader = create_tile_dataloader(
-        csv_file=val_csv, batch_size=args.batch_size, is_train=False
-    )
+    train_dataloader = create_tile_dataloader(src_dir=train_dir, batch_size=args.batch_size, is_train=True)
+    val_dataloader = create_tile_dataloader(src_dir=val_dir, batch_size=args.batch_size, is_train=False)
     # Model
     logger.info("Constructing model")
     model = LitResnet(num_classes=1, lr=args.lr)
@@ -51,20 +48,21 @@ def main(args):
     )
 
     # Logger
-    tb_logger = TensorBoardLogger("data/outputs/tb_logs", name="resnet", prefix=timestamp)
+    tb_logger = TensorBoardLogger(checkpoint_dir, name="dig_patho")# , prefix=timestamp)
 
     # Trainer
     trainer = pl.Trainer(
         max_epochs=args.max_epochs,
         callbacks=[checkpoint_callback, early_stopping_callback],
         logger=tb_logger,
-        # To use with the iteratable dataloader
-        limit_train_batches=100, limit_val_batches=10,
+        # To use with the infinite dataloader
+        # limit_train_batches=100, limit_val_batches=10,
+
         # gpus=1 if torch.cuda.is_available() else 0,  accelerator='dp' if torch.cuda.is_available() else None
     )
 
     # Train
-    trainer.fit(model, train_loader, val_loader)
+    trainer.fit(model, train_dataloader, val_dataloader)
 
 if __name__ == '__main__':
     logger = logging.getLogger("Trainer")
@@ -77,8 +75,8 @@ if __name__ == '__main__':
     logger.addHandler(sh)
 
     parser = argparse.ArgumentParser(description='Train ResNet model on WSI dataset')
-    parser.add_argument('--train_csv', type=str, default='tupac_16_100/data.csv', help='CSV file with training labels')
-    parser.add_argument('--val_csv', type=str, default='tupac_16_100/data.csv', help='CSV file with validation labels')
+    parser.add_argument('--train_path', type=str, default='tupac16/train', help='dir with pickle of train')
+    parser.add_argument('--val_path', type=str, default='tupac16/val', help='dir with pickle of val')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training and validation')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--max_epochs', type=int, default=50, help='Maximum number of epochs')
